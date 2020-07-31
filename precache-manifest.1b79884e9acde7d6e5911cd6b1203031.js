d302f8a735be2d66915d8ed98cd8941bf742181e
hector-pereyra <hpereyra27@gmail.com>
2020-07-31 15:47:40 -0400

    Updates

---
+++ precache-manifest.1b79884e9acde7d6e5911cd6b1203031.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "943c4383320233a36a03c01ead10fc5a",
+    "url": "/vikingfood/index.html"
+  },
+  {
+    "revision": "8f00ea52d4261e573398",
+    "url": "/vikingfood/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "958ceaa4978ceb0e0d56",
+    "url": "/vikingfood/static/css/main.7393f3a9.chunk.css"
+  },
+  {
+    "revision": "8f00ea52d4261e573398",
+    "url": "/vikingfood/static/js/2.6b9eb35d.chunk.js"
+  },
+  {
+    "revision": "618d9e067b5f7c0ee048a43ae1109b7d",
+    "url": "/vikingfood/static/js/2.6b9eb35d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "958ceaa4978ceb0e0d56",
+    "url": "/vikingfood/static/js/main.953da146.chunk.js"
+  },
+  {
+    "revision": "413257dd6f00d69ebe88",
+    "url": "/vikingfood/static/js/runtime-main.a830aa30.js"
+  },
+  {
+    "revision": "ade266074bff9449e3984636d2163a35",
+    "url": "/vikingfood/static/media/LOGOBLACK.ade26607.png"
+  },
+  {
+    "revision": "3f3749213993e27883ecaad5ed82f659",
+    "url": "/vikingfood/static/media/bluecrabicon.3f374921.png"
+  },
+  {
+    "revision": "b79c660aa9082cd64d19cd670949aceb",
+    "url": "/vikingfood/static/media/callidown.b79c660a.png"
+  },
+  {
+    "revision": "a6cec5136f6c7c657b240b0af23608c6",
+    "url": "/vikingfood/static/media/calliup.a6cec513.png"
+  },
+  {
+    "revision": "c931829643d7647caf97233244f3c530",
+    "url": "/vikingfood/static/media/cangrejoabout.c9318296.png"
+  },
+  {
+    "revision": "2f3995a8bf82b4c2c6c040b7682a9071",
+    "url": "/vikingfood/static/media/claws.2f3995a8.png"
+  },
+  {
+    "revision": "6fe63806ca2ccb4f63fbd66cf8c757b7",
+    "url": "/vikingfood/static/media/home.6fe63806.jpg"
+  },
+  {
+    "revision": "5a473810cd0b4d7e7b6c6b3e76e0b680",
+    "url": "/vikingfood/static/media/lamp.5a473810.png"
+  },
+  {
+    "revision": "db3bcdad1138b0a32e3a36b011dec3c3",
+    "url": "/vikingfood/static/media/mission.db3bcdad.png"
+  },
+  {
+    "revision": "efa5e2b10ce60b17ae3b9a5c149a9df2",
+    "url": "/vikingfood/static/media/navmenu.efa5e2b1.jpg"
+  },
+  {
+    "revision": "1217cc538173c19b6f1ab2267d9e6e3d",
+    "url": "/vikingfood/static/media/phylosophy.1217cc53.png"
+  }
+]);
